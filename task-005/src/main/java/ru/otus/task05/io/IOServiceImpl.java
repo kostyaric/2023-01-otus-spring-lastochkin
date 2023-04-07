cde0abadb928b2a95bd6cf3cf452e0616841aac3
K.Lastochkin <kostyaric@mail.ru>
2023-04-07 10:41:06 +0300

    Scaner

--- task-005/src/main/java/ru/otus/task05/io/IOServiceImpl.java
+++ task-005/src/main/java/ru/otus/task05/io/IOServiceImpl.java
@@ -4,28 +4,22 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.util.Scanner;
 
 @Service
 public class IOServiceImpl implements IOService {
 
     private final PrintStream printStream;
-    private final BufferedReader reader;
+    private final Scanner scanner;
 
     public IOServiceImpl (@Value("#{T(java.lang.System).out}") PrintStream printStream, @Value("#{ T(java.lang.System).in}") InputStream inputStream) {
         this.printStream = printStream;
-        this.reader = new BufferedReader(new InputStreamReader(inputStream));
+        this.scanner = new Scanner(inputStream);
     }
 
     @Override
     public String readMessage() {
-        String userInput = "";
-        try {
-            userInput = reader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("");
-        }
-        return userInput;
+        return scanner.nextLine();
     }
 
     @Override
